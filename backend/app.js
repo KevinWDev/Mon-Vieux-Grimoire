2429e1271016791e9432f9c25ddac925d2e7d286
Kevin W <kevinwatteau@outlook.fr>
2023-05-30 15:20:53 +0200

    Ajout de la gestion des fichiers utilisateur

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const bookRoutes = require('./routes/book')
 const userRoutes = require('./routes/user')
+const path = require('path');
 
 const app = express();
 
@@ -21,7 +22,8 @@ app.use((req, res, next) => {
     next();
   });
 
-app.use('/api/book', bookRoutes)
+app.use('/api/books', bookRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
