c60baaf57dea63cb9240f3239018b8915d93849b
Kevin W <kevinwatteau@outlook.fr>
2023-05-30 08:44:37 +0200

    Ajout système d'authentification

---
+++ backend/routes/book.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+
+const bookCtrl = require('../controllers/book');
+
+router.post('/', auth, bookCtrl.createBook);
+router.put('/:id', auth, bookCtrl.modifyBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
+router.get('/:id', auth, bookCtrl.getOneBook);
+router.get('/', auth, bookCtrl.getAllBook);
+
+module.exports = router;
